fce3260cd55acc3cb7ac57e37acdaa18079b5618
NureZibnytskaMariia <mariia.zibnytska@nure.ua>
2026-06-12 16:26:13 +0300

    Fix import path for api in LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import api from '../pages/api';
+import api from '../api';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
