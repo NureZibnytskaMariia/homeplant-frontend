34f195763af06b6e8559cc93a464a5f5119fb1c3
NureZibnytskaMariia <mariia.zibnytska@nure.ua>
2026-06-12 14:18:25 +0300

    Change API base URL to production endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://homeplant-backend-cvdnb5a8cff5dhdp.polandcentral-01.azurewebsites.net/api/',
 });
 
 // Додаємо токен автоматично до кожного запиту
